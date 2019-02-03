40087596fdcf8e9c135ef1b397a3134d85f08c32
Dilip Meghwal <er.dkmeghwal@gmail.com>
2019-02-03 13:58:27 +0530

    commit full project

---
+++ src/test/java/com/duke/utilities/TestUtils.java
@@ -0,0 +1,58 @@
+package com.duke.utilities;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import com.duke.base.testBase;
+
+public class TestUtils extends testBase{
+	private static TestUtils instance = null;
+	
+	private TestUtils() {
+		
+	}
+	
+	public static TestUtils getInstance() {
+		if(instance == null) {
+			instance = new TestUtils();
+		}
+		return instance;
+	}
+	
+	public String projPath = System.getProperty("user.dir");
+	
+	//To capture screenshot
+	public void captureScreenshotAsJPG(String fileName) throws IOException {
+        TakesScreenshot scrShot =((TakesScreenshot)driver);
+        File SrcFile=scrShot.getScreenshotAs(OutputType.FILE);
+        File destFile = new File(projPath + "\\screenShots\\" + TestUtils.getInstance().getTimeStamp() + "_" + fileName + ".jpg");
+		FileUtils.copyFile(SrcFile, destFile);
+	}
+	public void captureScreenshotAsJPEG(String fileName) throws IOException {
+        TakesScreenshot scrShot =((TakesScreenshot)driver);
+        File SrcFile=scrShot.getScreenshotAs(OutputType.FILE);
+        File destFile = new File(projPath + "\\screenShots\\" + TestUtils.getInstance().getTimeStamp() + "_" + fileName + ".jpeg");
+		FileUtils.copyFile(SrcFile, destFile);
+	}
+	public void captureScreenshotAsPNG(String fileName) throws IOException {
+        TakesScreenshot scrShot =((TakesScreenshot)driver);
+        File SrcFile=scrShot.getScreenshotAs(OutputType.FILE);
+        File destFile = new File(projPath + "\\screenShots\\" + TestUtils.getInstance().getTimeStamp() + "_" + fileName + ".png");
+		FileUtils.copyFile(SrcFile, destFile);
+	}
+	
+	//generate time stamp
+	public String getTimeStamp() {
+		Calendar cal = Calendar.getInstance();
+		SimpleDateFormat f = new SimpleDateFormat("MM_dd_yyyy_HH_mm_ss_SSSSSS");
+		String timeStamp = f.format(cal.getTime());
+		return timeStamp;
+	}
+}
